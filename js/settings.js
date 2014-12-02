d1a5a5d006346ca27ebc60dfba58b2559f23cb75
intrepidwebstudio <sunil@inpws.com>
2014-12-02 17:50:22 +0530

    456

--- js/settings.js
+++ js/settings.js
@@ -77,7 +77,7 @@ var	eboo_facebook;
         }
         function navigateTo(url,click_id,this_id,article_share_title){
 			
-		  iabRef = window.open(url, '_blank', 'toolbarposition=bottom,location=no,presentationstyle=formsheet,closebuttoncaption=close,backgroundcolor=red');//presentationstyle: Set to pagesheet, formsheet or fullscreen 
+		  iabRef = window.open(url, '_blank', 'toolbarposition=bottom,location=no,presentationstyle=formsheet,closebuttoncaption=Close');//presentationstyle: Set to pagesheet, formsheet or fullscreen 
 	      $.support.cors = true;
 		  this_id.style.setProperty( 'background-color', '#DFE3E7', 'important' );
 	  	  $.post(ajax_path+'clickcount.php?id='+click_id + '&user_id='+EbooUSER_ID+'&search_query_id='+SearchQueryId+'&feed_conducted='+feed_conducted+'&security_token = '+ security_token);
