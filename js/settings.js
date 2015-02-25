6b6259c390fcba32d7d529feaea9a49a8974f3bb
intrepidwebstudio <sunil@inpws.com>
2015-02-25 17:14:04 +0530

    12223

--- js/settings.js
+++ js/settings.js
@@ -1,85 +1,39 @@
-// JavaScript Document
-  
-//*************************************** settings start here here *************************************** 
-//var imagepath = 'http://ebbu.kurinchilion.com/';  				 //change to this    http://www.domainname.com/      (no need of /ebbu if using direct domain)
-//var ajax_path = 'http://ebbu.kurinchilion.com/app/';			// change to this 	http://www.domainname.com/app    (no need of /ebbu if using direct domain)
- 
-var imagepath = 'http://192.168.1.4/ebbu/';  				 //change to this    http://www.domainname.com/      (no need of /ebbu if using direct domain)
-var ajax_path = 'http://192.168.1.4/ebbu/app/';			// change to this 	http://www.domainname.com/app    (no need of /ebbu if using direct domain)
- 
- 
+var imagepath = 'http://ebbu.kurinchilion.com';  			
+var ajax_path = 'http://ebbu.kurinchilion.com/app/';		
 var pageCount = 0;
 var contentCount = 10;
-
 var search_pageCount = 0;
 var search_contentCount = 10;
 //*************************************** settings complete here ***************************************
-
 var new_skip_rss_count=0;	
 var new_skip_tweet_count=0;
-
-  var security_token=543219588865478555;
-
-   var SearchQueryId;
-   
-   var EbbuUSER_ID;
-   
-   
-   
-   
-   var selected_article=0;
-   var selected_article_id=0;
-   var slected_source_name;
-   
-   
-   
+var security_token=543219588865478555;
+var SearchQueryId;
+var EbbuUSER_ID;
+var selected_article=0;
+var selected_article_id=0;
+var slected_source_name;
 var	selected_category_id=0;
 var	selected_publication_id=0;
-   
-   
-   
-   
-   
-	var feedData;
-   var feed_conducted=0; 
-	
-   var user_time = 0;
-
-   var user_active = 0;
-
-   var global_article_title='';	
-	var global_article_link='';
-	
-	var Global_share_titleee ='';
-	var Global_share_linkkk ='';
-	var last_search;		
-	
-
-	var	shownrssarticle=0;
-	var	showntwitterarticle=0;
-		
-	
-	
-	
-
+var global_rating=0;   
+var feedData;
+var feed_conducted=0; 
+var user_time = 0;
+var user_active = 0;
+var global_article_title='';	
+var global_article_link='';
+var Global_share_titleee ='';
+var Global_share_linkkk ='';
+var last_search='';		
+var	shownrssarticle=0;
+var	showntwitterarticle=0;
 var	ebbu_twitter;
-
-
-var	ebbu_facebook;		
-	
-	        // Global InAppBrowser reference
+var	ebbu_facebook;
+var front_page_search='';		
         var iabRef = null;
 	    function iabLoadStart(event) {
-			
-		//	StatusBar.backgroundColorByHexString("#FFF");
-			
-		//	window.plugins.spinnerDialog.show();
-//			$('#loaderScreen').show();	
         }
 	   function iabLoadStop(event) {
-		   
-	//	   	window.plugins.spinnerDialog.hide();
-//			$('#loaderScreen').hide();
 		}
         function iabClose(event) {
           iabRef.removeEventListener('loadstart', iabLoadStart);
@@ -89,32 +43,19 @@ var	ebbu_facebook;
 		  iabRef.close();
         }
         function iabShare(event){
-          //  alert('SHARE:'+event.type);
-         //   iabRef.removeEventListener('loadstart', iabLoadStart);
-          //  iabRef.removeEventListener('loadstop', iabLoadStop);
-           // iabRef.removeEventListener('exit', iabClose);
-           // iabRef.removeEventListener('share', iabClose);
         }
-        function navigateTo(url,click_id,this_id,article_share_title){
-			
+        function navigateTo(url,click_id){
 			console.log(url);
-			
-		  iabRef = window.open(url, '_blank','toolbarposition=bottom,location=no,presentationstyle=formsheet,closebuttoncaption=Close');//presentationstyle: Set to pagesheet, formsheet or fullscreen 
-	      $.support.cors = true;
-		  var news_source=$('#id'+click_id).attr('data-value4');
-		  console.log('data-value4='+news_source);
-		  
-		  
-		 // $('#'+click_id).style.setProperty( 'background-color', '#DFE3E7', 'important' );
-		  
-			 $('#id'+click_id).attr('style', 'background-color:#DFE3E7 !important');
-		  
-	  	  $.post(ajax_path+'clickcount.php?id='+click_id + '&user_id='+EbbuUSER_ID+'&search_query_id='+SearchQueryId+'&feed_conducted='+feed_conducted+'&news_source='+news_source+'&security_token='+security_token);
+			iabRef = window.open(url, '_blank','toolbarposition=bottom,location=no,presentationstyle=formsheet,closebuttoncaption=Close');//presentationstyle: Set to pagesheet, formsheet or fullscreen 
+			$.support.cors = true;
+			var news_source=$('#id'+click_id).attr('data-value4');
+			console.log('data-value4='+news_source);
+	        $('#id'+click_id).attr('style', 'background-color:#DFE3E7 !important');
+	        $.post(ajax_path+'clickcount.php?id='+click_id + '&user_id='+EbbuUSER_ID+'&search_query_id='+SearchQueryId+'&feed_conducted='+feed_conducted+'&news_source='+news_source+'&security_token='+security_token);
 			iabRef.addEventListener('exit', iabClose);
             iabRef.addEventListener('share', iabShare);
             iabRef.addEventListener('loadstart', iabLoadStart);
             iabRef.addEventListener('loadstop', iabLoadStop);
-
         }
 		
 		
--- js/swipeleft.js
+++ js/swipeleft.js
@@ -1,133 +1,73 @@
-
-
-
-
-
 var hexDigits = new Array
         ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
-
-
-
 //Function to convert hex format to a rgb color
 function rgb2hex(rgb) {
  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
- 
- 
- //console.log("#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]));
 }
-
-
 function hex(x) {
   return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
  }
-
 // JavaScript Document
  $(function() {
-
     function prevent_default(e) {
         e.preventDefault();
     }
-
     function disable_scroll() {
         $(document).on('touchmove', prevent_default);
     }
-
     function enable_scroll() {
         $(document).unbind('touchmove', prevent_default)
     }
-
-
-
-
-   
     var x;
-	
 	var new_rect;
-	
 	var bgcolor;
-	
-    $('.swipe-delete li > a')
+	$('.swipe-delete li > a')
         .on('touchstart', function(e) {
-			
-					
 					if($(this).hasClass('leftitem'))
 					{
-						
-						
-						
-						
 						}
 					else{	
-					
-			$( ".swipe-delete li > a" ).each(function( index ) {
-					var rect = this.getBoundingClientRect();
-								if(rect.left !=0 )
-								{
-								$(this).animate({'left':'0px'},200,'linear',function(){  
-												
-												 new_rect = this.getBoundingClientRect();
-												 
-												 $(this).removeClass('leftitem');
-												 
-										}) // close em all
-								}
-	
-});
+									
+							$( ".swipe-delete li > a" ).each(function( index ) {
+									var rect = this.getBoundingClientRect();
+												if(rect.left !=0 )
+												{
+												$(this).animate({'left':'0px'},200,'linear',function(){  
+																 new_rect = this.getBoundingClientRect();
+																 $(this).removeClass('leftitem');
+														}) // close em all
+												}
+							
+							});
 			}
-			
 		    x = e.originalEvent.pageX // anchor point
-			
-		//	console.log(x);
+
 		})
         .on('touchmove',  function(e) {
 											if( $(this).hasClass('leftitem') == false )	
 											{
-			
 												var change = e.originalEvent.pageX - x;
-												//console.log(change);
 												change = Math.min(Math.max(-300, change), 0 ) // restrict to -100px left, 0px right
-											//	console.log("change = "+change);
 												if(change < -50)
 												{
-											//	e.currentTarget.style.setProperty( 'background-color', '#DFE3E7', 'important' );	
 												disable_scroll(); // disable scroll once we hit 10px horizontal slide
 												e.currentTarget.style.left = change + 'px';
 												}else{
-											//	e.currentTarget.style.left = 0 + 'px';
-											//	enable_scroll();	
 													}
 											}else{
-												
-										//	console.log( $(this).hasClass('leftitem') );
-												
 												var change1 = e.originalEvent.pageX - x;
-																																		
-												//console.log(change);
 												change1 = Math.min(Math.max(-231, change1), Math.max(231, change1) )
-											//	change = Math.min( Math.max(-100, change),Math.max(-100,change)) // restrict to -100px left, 0px right
-										//		console.log("change leftiem = "+change1);
-										//	e.currentTarget.style.setProperty( 'background-color', '#DFE3E7', 'important' );	
 												disable_scroll(); // disable scroll once we hit 10px horizontal slide
 												change1 = -231 + change1;
-										//		console.log("negative value = "+change1);
-											
 												e.currentTarget.style.left =  change1 + 'px';
-												
-												
 												}
 		})
         .on('touchend', function(e) {
-		 //	alert($(this).hasClass('listitem') );
-			
-		//	console.log($(this).hasClass('leftitem'));
-			
 		if( $(this).hasClass('leftitem') == true )
 		{	
-	//	alert('aaa');
 			var left = "0";
 	        left = parseInt(e.currentTarget.style.left)
- 			//	console.log(" LEFTTTT  =" + left );
 			if(left < -150)
 			{
 	     	var new_left = -231 + 'px';
@@ -140,21 +80,14 @@ function hex(x) {
 			e.currentTarget.style.left = new_left;		
 			}			
             enable_scroll()
-	//	console.log( "Final value ="+ $(this).hasClass('leftitem') );
-		
 		}else{
 			
 			var left = "0";
 	         left = parseInt(e.currentTarget.style.left)
-		//	console.log(" LEFTTTT  =" + left );
 			if(left < -90)
 			{
 	     	var new_left = -231 + 'px';
-			
-			
-			
 			e.currentTarget.style.left = new_left;	
-			
 			$(this).addClass('leftitem');
 			var click_id = $(this).attr('data-value3');
 			var news_source = $(this).attr('data-value4'); 
@@ -166,156 +99,102 @@ function hex(x) {
 			e.currentTarget.style.left = new_left;		
 			}			
             enable_scroll()
-			
 		}
-		//	console.log('new keft  ='+ new_left);
-		//e.currentTarget.addClass('tocuhedd');
 	    });
-		
-		
-		
 		$('.socialicon').on('touchstart', function(e) {
-		//console.log('touch started');	
 		$(this).children('.image_icon').css("opacity",".6");
-	//	console.log("staart Poistion = "+ y);
 			}).on('touchmove',  function(e) {
 		})
         .on('touchend', function(e) {
-				//console.log('touch_end');
 				$(this).children('.image_icon').css("opacity","1.0");
 	  		 $.getScript("js/SocialSharing.js");
 		 	 SocialSharing();
-				
 			var title_aaa =	$(this).attr('data-value');
 			var title_linkkk = $(this).attr('data-value1');
 			var media_link = $(this).attr('data-value2');
 			var news_source= $(this).attr('data-value4');
-			
-			
 			Global_share_titleee = title_aaa;
 			Global_share_linkkk = title_linkkk;
-			
 		if( media_link == "twitter" )
 			{
 				if( ebbu_twitter == "1" ){
 			window.plugins.socialsharing.shareViaTwitter("Found on @EbbuApp:  "+title_aaa, null /* img */,title_linkkk);	
 				}else{
-	
-	//callAlertAllow();
-					
 			 navigator.notification.confirm(
             'To share this article, we need access to your account.',  // message
 			ontwitterconfirm,
             'Twitter',            // title
             'Cancel,Allow'          // buttonLabels
         );
-			
-					
-				}
-				}
-			else if( media_link == "facebook"  )
-			{
-				
-				if( ebbu_facebook == "1" ){
-			window.plugins.socialsharing.shareViaFacebook("Found via EbbuApp.com:  ", null /* img */,title_linkkk);
-				}else{
-					
-  navigator.notification.confirm(
-            'To share this article, we need access to your account.',  // message
-			onfacebookconfirm,
-            'Facebook',            // title
-            'Cancel,Allow'          // buttonLabels
-        );
-					
-		
+	  }
 	}
-				
-				
-				
-				}
-			else if( media_link == "email"  )
+	else if( media_link == "facebook"  )
 			{
-				
-				
-					
+				if( ebbu_facebook == "1" ){
+						window.plugins.socialsharing.shareViaFacebook("Found via EbbuApp.com:  ", null /* img */,title_linkkk);
+				}else{
+						navigator.notification.confirm(
+						'To share this article, we need access to your account.',  // message
+						onfacebookconfirm,
+						'Facebook',            // title
+						'Cancel,Allow'          // buttonLabels
+						);
+	}
+}
+else if( media_link == "email"  )
+{
 window.plugins.socialsharing.shareViaEmail(
   'I found this article on <a href="http://www.EbbuApp.com"> EbbuApp.com </a> and thought of you. Check it out. '+'<p><a href="'+title_linkkk+'" >'+title_aaa +'</a></p>', // can contain HTML tags, but support on Android is rather limited:  http://stackoverflow.com/questions/15136480/how-to-send-html-content-with-image-through-android-default-email-client
   'Check this out...',
-null, // TO: must be null or an array
+  null, // TO: must be null or an array
   null, // CC: must be null or an array
   null, // BCC: must be null or an array
   null // FILES: can be null, a string, or an array
-  
-			);					
-		
-		}
-				
-				
+		);					
+	   }
     });
  });
 
 function onfacebookconfirm(button1){
 	if(button1 == 2)
 	{
-			 change_value = '1';
+	    change_value = '1';
 		$('#facebooklogout').css('display','none');
 		$('#facebooklogged').css('display','block');
-
 		var socialName = 'facebook';
 		db.transaction( function(tx){ updatevalues(tx, change_value,socialName)}, ErrorCallBack );	
-		
-	window.plugins.socialsharing.shareViaFacebook("Found via EbbuApp.com:  "+Global_share_titleee, null /* img */,Global_share_linkkk);
-		
+		window.plugins.socialsharing.shareViaFacebook("Found via EbbuApp.com:  "+Global_share_titleee, null /* img */,Global_share_linkkk);
 	}
-	
-	
 }
-
 function ontwitterconfirm(button2)
 {
-	
 	if(button2 == 2)
 	{
 			 change_value = '1';
 		$('#twitterlogout').css('display','none');
 		$('#twitterlogged').css('display','block');
-
 		var socialName = 'twitter';
 		db.transaction( function(tx){ updatevalues(tx, change_value,socialName)}, ErrorCallBack );	
-		
 		window.plugins.socialsharing.shareViaTwitter("Found on @EbbuApp:  "+Global_share_titleee, null /* img */,Global_share_linkkk);	
-		
 	}
-	
-
-	
-	
-	}
-
-
-
-
-
+}
 
 var y;
 var new_position;
 
-
-
-$("#search_show_bar").on('click', function() {
-   
+$(".search_show_bar").on('click', function() {
 	  show_search();
 	
 });
 
 
-$('#showsearch').on('click', function(e) {
+$('.showsearch').on('click', function(e) {
 	show_search();
 });
 
 
 
-$('#showsearch').on('touchstart', function(e) {
+$('.showsearch').on('touchstart', function(e) {
 	$(this).children('.searchimg').css("opacity",".6");
 })
 .on('touchmove',function(){
@@ -323,9 +202,6 @@ $('#showsearch').on('touchstart', function(e) {
 .on('touchend',function(){
 	$(this).children('.searchimg').css("opacity","1");
 	});
-	
-	
-	
 	
 
 $('#settingspage').on('touchstart', function(e) {
@@ -350,61 +226,29 @@ $(this).children('.backimg').css("opacity",".6");
 $(this).children('.backimg').css("opacity","1");
 });
 	
-
-
-
-//db.transaction(updatesettings, errorCB_settings);
-
-
-
-	
 	
 	function successSettings(){
 		
-		
-	//	alert("successsssss");
 		}
 	function errorSettings(err)
 	{
-		
-	//	alert("ERRORR = "+err.message);
-		
 		}
-		
 		
 			 function updatevalues(tx, change_value,socialName)
 			 {
-				 
-				 
-				 
 				if(socialName == "twitter") 
 				{ 
 				 ebbu_twitter = change_value;
-				// console.log("my values = "+ebbu_twitter);
 				 var myvalue = change_value;
 				 tx.executeSql('UPDATE ebbuUser SET twitter = ? ' ,[myvalue] , successSettings , errorSettings);
 				}
-			
-				
 				else if(socialName == "facebook")
 				{
-					
 				 ebbu_facebook = change_value;
-				// console.log("my values = "+ebbu_facebook);
 				 var myvalue = change_value;
 				 tx.executeSql('UPDATE ebbuUser SET facebook = ? ' ,[myvalue] , successSettings , errorSettings);
-	
-					
 					}		
-				
-				
-				
-				
-				
 			 }
-		
-		
-		
       var change_value;   
 	  
 	  
@@ -514,30 +358,13 @@ $(this).children('.backimg').css("opacity","1");
 	}
 
 
-
-
-
-
-
 var callback123 = function(buttonIndex) {
     setTimeout(function() {
-      // like other Cordova plugins (prompt, confirm) the buttonIndex is 1-based (first button is index 1)
-//      alert('button index clicked: ' + buttonIndex);
-	  
-	 
-		  
 		  $.getScript("js/SocialSharing.js");
 		  SocialSharing();
-		  
-		//  alert('title = ' + global_article_title + ' link  =  '+ global_article_link );
-	  
-	  
 	  if(buttonIndex == 1)
 	  {
-	
-		//  window.plugins.socialsharing.shareViaFacebook(  article_title, null, null, function(){alert("ok")}, function(e){alert("error: " + e)});
 		  window.plugins.socialsharing.shareViaEmail(global_article_title , null /* img */,global_article_link );
-		  
 		  }
 		 else if(buttonIndex == 2)
 		 {
@@ -553,48 +380,24 @@ var callback123 = function(buttonIndex) {
 			 }
 		 else if(buttonIndex == 5)
 		 {
-			 
-			 
 			 }
 		else if(buttonIndex == 6)
 		 {
 			 
 			 }	 
-		 
-	  
-	  
     });
   };
 
-
-
-
   function share_article() {
 	  
-	  
-	  
-	  
     var options = {
-       // 'title': 'What do you want with this image?',
         'buttonLabels': ['Twitter' ,'Facebook', 'Email','Send to Reading List','Open in Safari'],
         'androidEnableCancelButton' : false, // default false
         'winphoneEnableCancelButton' : false, // default false
         'addCancelButtonWithLabel': 'Cancel',
-//        'addDestructiveButtonWithLabel' : 'Delete it'
     };
-    // Depending on the buttonIndex, you can now call shareViaFacebook or shareViaTwitter
-    // of the SocialSharing plugin (https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin)
     window.plugins.actionsheet.show(options, callback);
   };
-  
- 
-
-
-$("#search_show_bar").live("tap", function(event){
-	
+$(".search_show_bar").live("tap", function(event){
 	show_search();
-  
 });
-
-
-
